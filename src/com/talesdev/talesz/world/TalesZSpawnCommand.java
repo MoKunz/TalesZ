aa29dd09ab36dcbd17380bf1e5b2d3fb3095c0da
BuildTools <unconfigured@null.spigotmc.org>
2015-03-12 14:28:43 +0700

    Fixed syntax error

--- src/com/talesdev/talesz/world/TalesZSpawnCommand.java
+++ src/com/talesdev/talesz/world/TalesZSpawnCommand.java
@@ -1,16 +1,18 @@
 package com.talesdev.talesz.world;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class TalesZSpawnCommand implements CommandExecutor {
-    @override
-    public boolean onCommand(CommandSender sender,Command comand,String label,String[] args){
-        if(command.getName().equalsIgnoreCase("taleszspawn"){
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("taleszspawn")) {
             if(sender instanceof Player){
                 Player player = (Player) sender;
-                boolean result = player.teleport(TalesZWorld.getSpawnPointManager().ramdomSpawnLocation());
+                boolean result = player.teleport(TalesZWorld.getSpawnPointManager().randomSpawnLocation());
                 // teleport successful
                 if(result){
                     player.sendMessage(ChatColor.GREEN + "You have been teleported to TalesZ World!");
@@ -23,8 +25,10 @@ public class TalesZSpawnCommand implements CommandExecutor {
             else{
                 sender.sendMessage(ChatColor.RED + "Console can't warp!");
             }
+            return true;
         }
         else{
             return false;
         }
     }
+}
